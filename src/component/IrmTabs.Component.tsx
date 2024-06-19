acdf8aa12319d068462d8d3667d121d81e32c19f
MeghaiRM-B <meghashree.b@irmcloud.info>
2024-06-19 18:29:13 +0530

    tabs

---
+++ src/component/IrmTabs.Component.tsx
@@ -0,0 +1,41 @@
+import { ConfigProvider, Tabs } from "antd";
+import { ReactNode, MouseEvent } from "react";
+
+export interface Tab {
+  key: string;
+  label: ReactNode;
+  children?: ReactNode;
+  disabled?: boolean;
+  closable?: boolean;
+  forceRender?: boolean;
+  destroyInactiveTabPane?: boolean;
+}
+
+export interface TabsProps {
+  tabPosition: "top" | "right" | "bottom" | "left";
+  items: Tab[];
+  type?: "line" | "card";
+  size?: "large" | "middle" | "small";
+  tabBarGutter?: number;
+  activeKey?: string;
+  icon?: ReactNode;
+  addIcon?: React.ReactNode;
+  animated?: boolean;
+  centered?: boolean;
+  defaultActiveKey?: string;
+  indicator?: { size?: number; align?: "start" | "center" | "end"; };
+  onChange?: (activeKey: string) => void;
+}
+
+const IrmTabs = (props: TabsProps) => {
+  const { ...restProps } = props;
+  return (
+    <ConfigProvider>
+    <Tabs
+    {...restProps}
+  ></Tabs>
+  </ConfigProvider>
+  );
+}
+
+export default IrmTabs;
